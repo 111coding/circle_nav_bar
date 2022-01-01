9b9fc7d49edc19e08e7861ad0c373e7f37bded72
이지원 <jwlee@ijiwon-ui-MacBookPro.local>
2022-01-01 19:02:49 +0900

    File I

--- lib/circle_bottom_bar.dart
+++ lib/circle_bottom_bar.dart
@@ -1,6 +1,5 @@
 library circle_bottom_bar;
 
-import 'package:circle_bottom_bar/circle_bottom_painter.dart';
 import 'package:flutter/material.dart';
 
 class CircleBottomBar extends StatefulWidget {
@@ -114,7 +113,7 @@ class _CircleBottomBarState extends State<CircleBottomBar> with TickerProviderSt
                         child: Container(
                           width: widget.circleWidth,
                           height: widget.circleWidth,
-                          transform: Matrix4.translationValues(0, -(widget.circleWidth * 0.5) + CircleBottomPainter.getMiniRadius(widget.circleWidth) - widget.circleWidth * 0.5 * (1 - activeIconAc.value), 0),
+                          transform: Matrix4.translationValues(0, -(widget.circleWidth * 0.5) + _CircleBottomPainter.getMiniRadius(widget.circleWidth) - widget.circleWidth * 0.5 * (1 - activeIconAc.value), 0),
                           // color: Colors.amber,
                           child: widget.activeIcons[index],
                         ),
@@ -122,7 +121,7 @@ class _CircleBottomBarState extends State<CircleBottomBar> with TickerProviderSt
                 ],
               ),
             ),
-            painter: CircleBottomPainter(
+            painter: _CircleBottomPainter(
               iconWidth: widget.circleWidth,
               color: widget.color,
               xOffsetPercent: tabAc.value,
@@ -136,4 +135,109 @@ class _CircleBottomBarState extends State<CircleBottomBar> with TickerProviderSt
       ),
     );
   }
+}
+
+class _CircleBottomPainter extends CustomPainter {
+  _CircleBottomPainter({
+    required this.iconWidth,
+    required this.color,
+    required this.xOffsetPercent,
+    required this.boxRadius,
+    required this.shadowColor,
+    required this.elevation,
+    this.gradient,
+  });
+
+  final Color color;
+  final double iconWidth;
+  final double xOffsetPercent;
+  final BorderRadius boxRadius;
+  final Color shadowColor;
+  final double elevation;
+  final Gradient? gradient;
+
+  static double getR(double circleWidth) {
+    return circleWidth / 2 * 1.2;
+  }
+
+  static double getMiniRadius(double circleWidth) {
+    return getR(circleWidth) * 0.3;
+  }
+
+  static double convertRadiusToSigma(double radius) {
+    return radius * 0.57735 + 0.5;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Path path = Path();
+
+    Paint paint = Paint();
+    final w = size.width;
+    final h = size.height;
+    final r = getR(iconWidth);
+    final miniRadius = getMiniRadius(iconWidth);
+    final x = xOffsetPercent * w;
+    final firstX = x - r;
+    final secontX = x + r;
+
+    // TopLeft Radius
+    path.moveTo(0, 0 + boxRadius.topLeft.y);
+    path.quadraticBezierTo(0, 0, boxRadius.topLeft.x, 0);
+    path.lineTo(firstX - miniRadius, 0);
+    path.quadraticBezierTo(firstX, 0, firstX, miniRadius);
+
+    path.arcToPoint(
+      Offset(secontX, miniRadius),
+      radius: Radius.circular(r),
+      clockwise: false,
+    );
+
+    path.quadraticBezierTo(secontX, 0, secontX + miniRadius, 0);
+
+    // TopRight Radius
+    path.lineTo(w - boxRadius.topRight.x, 0);
+    path.quadraticBezierTo(w, 0, w, boxRadius.topRight.y);
+
+    // BottomRight Radius
+    path.lineTo(w, h - boxRadius.bottomRight.y);
+    path.quadraticBezierTo(w, h, w - boxRadius.bottomRight.x, h);
+
+    // BottomLeft Radius
+    path.lineTo(boxRadius.bottomLeft.x, h);
+    path.quadraticBezierTo(0, h, 0, h - boxRadius.bottomLeft.y);
+
+    path.close();
+
+    paint.color = color;
+
+    if (gradient != null) {
+      Rect shaderRect = Rect.fromCircle(center: Offset(w / 2, h / 2), radius: 180.0);
+      paint.shader = gradient!.createShader(shaderRect);
+    }
+
+    // canvas.drawShadow(path, shadowColor, elevation, false);
+    // Path oval = Path()..addOval(Rect.fromCircle(center: Offset(x, miniRadius), radius: iconWidth / 2));
+
+    // canvas.drawShadow(oval, shadowColor, elevation, false);
+    canvas.drawPath(
+        path,
+        Paint()
+          ..color = shadowColor
+          ..maskFilter = MaskFilter.blur(BlurStyle.normal, convertRadiusToSigma(elevation)));
+
+    canvas.drawCircle(
+        Offset(x, miniRadius),
+        iconWidth / 2,
+        Paint()
+          ..color = shadowColor
+          ..maskFilter = MaskFilter.blur(BlurStyle.normal, convertRadiusToSigma(elevation)));
+
+    canvas.drawPath(path, paint);
+
+    canvas.drawCircle(Offset(x, miniRadius), iconWidth / 2, paint);
+  }
+
+  @override
+  bool shouldRepaint(_CircleBottomPainter oldDelegate) => oldDelegate != this;
 }
